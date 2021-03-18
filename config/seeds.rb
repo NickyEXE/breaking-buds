7788d8628f58d5c6c9ec54e491521c87ea774581
NickyEXE <dovern42@gmail.com>
2021-03-18 18:27:18 -0400

    add seed data for message

--- config/seeds.rb
+++ config/seeds.rb
@@ -1,1 +1,6 @@
 BreakingBadAPI.new.response.each{|character_hash| Character.new(character_hash)}
+
+walt = Character.all.first
+walt.add_message("Happy teacher appreciation day")
+walt.add_message("Keep grinding!")
+walt.add_message("Hi there!")
